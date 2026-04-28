7b4a8b24a74e894a94e294b9cba07e69dbd13756
Balaanand26 <balaanand2606@gmail.com>
2026-04-28 10:54:43 +0530

    Upload loading

--- script.js
+++ script.js
@@ -125,3 +125,27 @@ document.addEventListener("DOMContentLoaded", () => {
     popup.classList.remove("show");
   });
 });
+
+
+// Counter 
+
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current == end) {
+          clearInterval(timer);
+        }
+      }, step);
+  }
+  counter("count1", 0, 10, 3000);
+  counter("count2", 0, 50, 3000);
+  counter("count3", 0, 600, 3000);
+  counter("count4", 0, 100, 3000);
+});
